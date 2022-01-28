694805e9b074907b34cfb800c08db75b27f87b9e
Victor Jurado <victorimagenehealth>
2022-01-28 13:42:01 -0600

    28012022

--- controllers/forms_controller.js
+++ controllers/forms_controller.js
@@ -2,5 +2,5 @@ exports.saveForm = (req, res) => {
 
     console.log(req.body)
 
-    res.status(200).send({ message: "we have received your data!!"})
+    res.status(200).send({ message: "We have received your data succesfully!!"})
 }
